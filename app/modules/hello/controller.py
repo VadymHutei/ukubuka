c7686f7210da2bd5c37f2908dc5a4828d9695618
Vadym Hutei <hutei@live.com>
2021-02-11 20:32:39 +0200

    deleted hello module

--- app/modules/hello/controller.py
+++
@@ -1,8 +0,0 @@
-class HelloController:
-
-    def __init__(self, view, foo):
-        self.view = view
-        print(foo)
-
-    def helloAction(self):
-        return self.view.render()
--- app/modules/hello/view.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template
-
-
-class HelloView:
-
-    def render(self):
-        return render_template('hello/page.html')
