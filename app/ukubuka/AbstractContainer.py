e303a294d6b1c8f104a76f0f5d09592bfbde641d
Vadym Hutei <hutei@live.com>
2021-02-16 22:29:41 +0200

    ukubuka

---
+++ app/ukubuka/AbstractContainer.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+from inspect import isclass
+
+
+class AbstractContainer(ABC):
+
+    _providers = []
+    _resources = {}
+
+    def __init__(self):
+        self._setProviders()
+        self._setResources()
+
+    def get(self, resourceName):
+        dependencies = [self.get(dependency.__name__) if isclass(dependency) else dependency for dependency in self._resources[resourceName]['dependencies']]
+        return self._resources[resourceName]['class'](*dependencies)
+
+    @abstractmethod
+    def _setProviders(self):
+        pass
+
+    def _addProvider(self, provider):
+        self._providers.append(provider)
+
+    def _setResources(self):
+        for provider in self._providers:
+            self._resources.update({resource.__name__: {'class': resource, 'dependencies': dependencies} for resource, dependencies in provider.getResources().items()})
