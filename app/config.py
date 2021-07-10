8f3b7160e9084fc2872ca6f3750f64511ed8e168
Vadym Hutei <hutei@live.com>
2021-07-10 19:05:09 +0300

    config

--- app/config.py
+++ app/config.py
@@ -1,10 +1,22 @@
-from pymysql.cursors import DictCursor
-
-
 DB_CREDENTIALS = {
     'host': 'localhost',
     'user': 'root',
     'password': 'root',
     'database': 'ukubuka',
-    'cursorclass': DictCursor
 }
+
+SESSION_COOKIE_NAME = 'usid'
+SESSION_ID_LENGTH = 32
+SESSION_LIFETIME_DAYS = 7
+
+PASSWORD_ABC = '1234567890abcdefghijklmnopqrstuvwxyz'
+PASSWORD_ABC_SAFE = '23456789abcdefghjkmnpqrstuvwxyz'
+PASSWORD_ABC_UPPER = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+PASSWORD_ABC_UPPER_SAFE = '23456789ABCDEFGHJKMNPQRSTUVWXYZ'
+PASSWORD_ABC_FULL = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+PASSWORD_ABC_FULL_SAFE = '23456789abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ'
+PASSWORD_LENGTH = 12
+PASSWORD_LENGTH_MIN = 1
+PASSWORD_LENGTH_MAX = 128
+PASSWORD_SALT_LENGTH = 12
+PASSWORD_SALT = 'gEXBniuxqTkO'
