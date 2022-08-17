27270628bef27607f9bfe43095f4db425db89b70
Vadym Hutei <hutei@live.com>
2022-08-17 13:03:39 +0300

    app config

--- app/main.py
+++ app/main.py
@@ -11,7 +11,10 @@ from modules.User.routes.UserACPBlueprint import userACPBlueprint
 from modules.User.routes.UserBlueprint import userBlueprint
 
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+
+app.config.from_object('config')
+app.config.from_pyfile('config.py')
 
 app.jinja_env.filters.update(filters)
 
@@ -27,35 +30,3 @@ app.register_blueprint(categoryACPBlueprint)
 app.register_blueprint(dashboardACPBlueprint)
 app.register_blueprint(translationsACPBlueprint)
 app.register_blueprint(userACPBlueprint)
-
-# @app.route('/<string:language>/catalog', methods=['GET'])
-# @languageRedirect
-# @withSession
-# def catalogMainPage():
-#     controller = CatalogController()
-#     return controller.catalogMainPageAction()
-
-# @app.route('/<string:language>/catalog/<string:catalogAlias>', methods=['GET'])
-# @languageRedirect
-# @withSession
-# def catalogPage(catalogAlias):
-#     controller = CatalogController()
-#     return controller.catalogAction(catalogAlias)
-
-# @app.route('/product/<productID>', methods=['GET'])
-# @languageRedirect
-# def productPage(productID):
-#     controller = ProductController()
-#     return controller.productAction(productID)
-
-# @app.route('/<string:language>/shop', methods=['GET'])
-# @languageRedirect
-# def shopPage():
-#     controller = ShopController()
-#     return controller.shopAction()
-
-# @app.route('/<string:language>/shop/<path:path>', methods=['GET'])
-# @languageRedirect
-# def shop(path):
-#     controller = ShopController()
-#     return controller.shopAction(path)
