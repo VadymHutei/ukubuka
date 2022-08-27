774edc97af64286048bf0b980db119a530efcf44
Vadym Hutei <hutei@live.com>
2022-08-27 16:04:34 +0300

    validation methods

--- app/modules/Ukubuka/UkubukaValidator.py
+++ app/modules/Ukubuka/UkubukaValidator.py
@@ -3,10 +3,31 @@ from vendor.ukubuka.validator import Validator
 
 class UkubukaValidator(Validator):
 
-    @staticmethod
-    def intID(ID, unsigned=True):
-        min = 0 if unsigned else -2147483648
-        max = 4294967295 if unsigned else 2147483647
+    INT_MIN_VALUE = -2147483648
+    INT_MAX_VALUE = 2147483647
+    UNSIGNED_INT_MIN_VALUE = 0
+    UNSIGNED_INT_MAX_VALUE = 4294967295
+
+    @classmethod
+    def DB_int(cls, ID: int, unsigned: bool = True) -> bool:
         if isinstance(ID, int):
-            return min <= ID <= max
+            if unsigned:
+                return cls.UNSIGNED_INT_MIN_VALUE <= ID <= cls.INT_MAX_VALUE
+            else:
+                return cls.INT_MIN_VALUE <= ID <= cls.INT_MAX_VALUE
+        return False
+
+    @classmethod
+    def int_ID(cls, ID: int) -> bool:
+        if isinstance(ID, int):
+            return cls.DB_int(ID, True)
+        return False
+
+    @classmethod
+    def text_ID(cls, ID: str) -> bool:
+        if isinstance(ID, str):
+            try:
+                return cls.DB_int(int(ID), True)
+            except:
+                return False
         return False
--- app/modules/User/validators/UserValidator.py
+++ app/modules/User/validators/UserValidator.py
@@ -4,19 +4,15 @@ from modules.Ukubuka.UkubukaValidator import UkubukaValidator
 
 
 class UserValidator(UkubukaValidator):
-    
+
     @staticmethod
-    def password(password):
+    def password(password: str) -> bool:
         if isinstance(password, str):
             return bool(re.fullmatch(r'[\w\W]{3,64}', password))
         return False
 
     @staticmethod
-    def textID(ID):
-        return UserValidator.intID(ID, True)
-
-    @staticmethod
-    def name(name):
+    def name(name: str) -> bool:
         if isinstance(name, str):
             return bool(re.fullmatch(r'[a-zA-Zа-яА-ЯєЄіІїЇґҐ\']{1,64}', name))
         return False
