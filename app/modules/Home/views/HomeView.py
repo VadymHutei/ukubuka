cf4864fb725801459e01f1f1f2ff27a76d815751
Vadym Hutei <hutei@live.com>
2022-09-25 19:52:53 +0300

    home view

--- app/modules/Home/views/HomeView.py
+++ app/modules/Home/views/HomeView.py
@@ -5,8 +5,4 @@ from modules.Base.views.ACPView import ACPView
 class HomeView(ACPView):
 
     template: str = 'modules/Home/homepage.html'
-
-    def _prepare_template_data(self):
-        super()._prepare_template_data()
-
-        self.template_data['title'] = g.t._('Ukubuka')
+    page_title: str = 'Ukubuka'
