691bc8bbc418e5cbadc4e4a2a86c6caa88194c1f
Vadym Hutei <hutei@live.com>
2023-01-25 00:27:29 +0200

    language controller

--- app/controllers/HTTP/ACP/LanguageController.py
+++ app/controllers/HTTP/ACP/LanguageController.py
@@ -1,10 +1,12 @@
 from services.LanguageService import LanguageService
+from di_container import di_container
 
 
 class LanguageController:
 
     def languages_page_action(self):
-        language_service = LanguageService()
+        language_service = di_container.get(LanguageService)
         languages = language_service.get_languages()
+        print(languages)
 
         return 'languages page'
