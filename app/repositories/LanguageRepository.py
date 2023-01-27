3e24c4cc4f57ada0085618779e25eda623ff1189
Vadym Hutei <hutei@live.com>
2023-01-28 00:36:54 +0200

    LanguageRepository

--- app/repositories/LanguageRepository.py
+++ app/repositories/LanguageRepository.py
@@ -1,7 +1,22 @@
-from repositories.AbstractRepository import AbstractRepository
+from repositories.MySQLRepository import MySQLRepository
 
 
-class LanguageRepository(AbstractRepository):
+class LanguageRepository(MySQLRepository):
+
+    TABLE = 'language'
 
     def find_all(self):
-        return []
+        query = f'''
+            SELECT
+                code,
+                name,
+                is_active,
+                is_default
+            FROM
+                {self.TABLE}
+        '''
+        with self.connection as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+        
+        return cursor.fetchall()
