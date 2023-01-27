ef31275d4a83a6919368fddd1ca94dec493e03ea
Vadym Hutei <hutei@live.com>
2023-01-28 00:08:53 +0200

    dependencies in service container

--- app/service_container/ServiceContainer.py
+++ app/service_container/ServiceContainer.py
@@ -5,23 +5,24 @@ class ServiceContainer:
 
     _services: dict = {}
 
-    def _create(self, service_class):
-        dependencies = {}
+    def _create(self, service_class: type, dependencies: dict = {}):
+        d = {}
+        d.update(dependencies)
         service_parametert = inspect.signature(service_class).parameters
         for parameter in service_parametert.values():
             if parameter.name == 'self':
                 continue
             if parameter.annotation == inspect._empty:
                 continue
-            dependencies[parameter.name] = self.get(parameter.annotation)
+            d[parameter.name] = self.get(parameter.annotation)
 
-        return service_class(**dependencies)
+        return service_class(**d)
 
-    def get(self, service_class):
+    def get(self, service_class: type, dependencies: dict = {}):
         if service_class in self._services:
             return self._services[service_class]
 
-        service = self._create(service_class)
+        service = self._create(service_class, dependencies)
         self._services[service_class] = service
 
         return self._services[service_class]
