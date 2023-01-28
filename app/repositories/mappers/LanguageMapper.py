dee645bd026b200cf1721e67505b428124e1ea1d
Vadym Hutei <hutei@live.com>
2023-01-28 15:14:51 +0200

    language mapper

---
+++ app/repositories/mappers/LanguageMapper.py
@@ -0,0 +1,22 @@
+from entities.Language import Language
+from repositories.mappers.IMapper import IMapper
+
+
+class LanguageMapper(IMapper):
+    
+    def from_row(self, row: dict) -> Language:
+        return Language(
+            code = row['code'],
+            name = row['name'],
+            is_active=row['is_active'],
+            is_default=row['is_default'],
+        )
+
+    def from_rows(self, rows: list|tuple) -> dict[str, Language]:
+        languages = {}
+
+        for row in rows:
+            language = self.from_row(row)
+            languages[language.code] = language
+
+        return languages
