ece1a6c600677f39a47bb547a32b5b46dc9f4c54
Vadym Hutei <hutei@live.com>
2023-01-29 21:01:11 +0200

    service container

--- app/service_container/ServiceContainer.py
+++ app/service_container/ServiceContainer.py
@@ -6,30 +6,32 @@ class ServiceContainer:
     _services_map: dict[type, type] = {}
     _services: dict = {}
 
-    def _create(self, service_class: type, dependencies: dict = {}):
-        d = {}
-        d.update(dependencies)
+    def _create(self, service_class: type, parameters: dict):
+        dependencies = {}
+        dependencies.update(parameters)
         service_parametert = inspect.signature(service_class).parameters
         for parameter in service_parametert.values():
+            if parameter.name in dependencies:
+                continue
             if parameter.name == 'self':
                 continue
             if parameter.annotation == inspect._empty:
                 continue
-            d[parameter.name] = self.get(parameter.annotation)
+            dependencies[parameter.name] = self.get(parameter.annotation)
 
-        return service_class(**d)
+        return service_class(**dependencies)
 
     def bind(self, services_map: dict[type, type]) -> None:
         self._services_map.update(services_map)
 
-    def get(self, service_class: type, dependencies: dict = {}):
+    def get(self, service_class: type, **parameters):
         if service_class in self._services_map:
             service_class = self._services_map[service_class]
 
         if service_class in self._services:
             return self._services[service_class]
 
-        service = self._create(service_class, dependencies)
+        service = self._create(service_class, parameters)
         self._services[service_class] = service
 
         return self._services[service_class]
