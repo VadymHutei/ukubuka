6fd6aefc3141bb5c63864616518979857e533167
Vadym Hutei <hutei@live.com>
2023-01-29 13:38:42 +0200

    MySQLRepository

--- app/repositories/MySQLRepository.py
+++ app/repositories/MySQLRepository.py
@@ -1,15 +1,14 @@
-import pymysql
+from pymysql import connect
+from pymysql.cursors import DictCursor
 from flask import current_app as app
 
-from repositories.AbstractRepository import AbstractRepository
 
-
-class MySQLRepository(AbstractRepository):
+class MySQLRepository:
 
     def _get_connection(self):
-        _db_credentials = app.config['MYSQL_DB_CREDENTIALS']
-        _db_credentials['cursorclass'] = pymysql.cursors.DictCursor
-        return pymysql.connect(**_db_credentials)
+        credentials = {"cursorclass": DictCursor}
+        credentials.update(app.config['MYSQL_DB_CREDENTIALS'])
+        return connect(**credentials)
 
     @property
     def connection(self):
