1964e3da01427e5063022c0637458c89f8ba8303
Vadym Hutei <hutei@live.com>
2023-09-14 00:25:15 +0300

    productView

--- app/views/web/ProductView.py
+++ app/views/web/ProductView.py
@@ -1,3 +1,4 @@
+from transformers.response_transformers.Product.ProductTransformer import ProductTransformer
 from views.web.WebView import WebView
 
 
@@ -6,3 +7,8 @@ class ProductView(WebView):
     _page_code: str = 'product'
 
     _with_layout: bool = True
+
+    def _prepare_page_data(self) -> None:
+        super()._prepare_page_data()
+    
+        self._data['product'] = ProductTransformer.transform(self._data['product'])
