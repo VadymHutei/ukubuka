77552a9681f3090d49f30c15e687ecd5a66189b9
Vadym Hutei <hutei@live.com>
2023-09-24 13:08:53 +0300

    webstore controllers

--- app/blueprints/ProductBlueprint.py
+++ app/blueprints/ProductBlueprint.py
@@ -4,7 +4,7 @@ from blueprints.blueprint_names import PRODUCT_BLUEPRINT
 from modules.Language.requestDecorators import language_redirect
 from modules.Session.requestDecorators import with_session
 from service_container import sc
-from controllers.ProductController import ProductController
+from controllers.webstore.ProductController import ProductController
 
 
 product_blueprint = Blueprint(PRODUCT_BLUEPRINT, __name__, url_prefix='/<string:language>/products')
