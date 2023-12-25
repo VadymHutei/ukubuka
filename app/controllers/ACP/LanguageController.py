c90fcf003d45c3df0df5ec65fd68761194863ce3
Vadym Hutei <hutei@live.com>
2023-12-25 21:58:21 +0200

    ACP controllers

--- app/controllers/ACP/LanguageController.py
+++ app/controllers/ACP/LanguageController.py
@@ -1,10 +1,7 @@
 from flask import g, redirect, request, url_for
-from werkzeug import Response
 
 from blueprints.blueprint_names import ACP_LANGUAGE_BLUEPRINT
 from controllers.IController import IController
-from data_transfer_objects.Language.AddLanguageDTO import AddLanguageDTO
-from data_transfer_objects.Language.UpdateLanguageDTO import UpdateLanguageDTO
 from services.Language.LanguageService import LanguageService
 from transformers.request_transformers.Language.AddLanguageDTOTransformer import AddLanguageDTOTransformer
 from transformers.request_transformers.Language.UpdateLanguageDTOTransformer import UpdateLanguageDTOTransformer
@@ -15,22 +12,22 @@ from views.HTML.ACP.Language.LanguagesView import LanguagesView
 
 class LanguageController(IController):
 
-    def __init__(self, service: LanguageService) -> None:
+    def __init__(self, service: LanguageService):
         self._service: LanguageService = service
 
-    def languages_page_action(self) -> str:
+    def languages_page_action(self):
         view = LanguagesView()
 
         view.set_data(languages=self._service.find_all())
 
         return view.render()
 
-    def add_language_page_action(self) -> str:
+    def add_language_page_action(self):
         view = AddLanguageView()
 
         return view.render()
 
-    def add_language_action(self) -> Response:
+    def add_language_action(self):
         add_language_dto = AddLanguageDTOTransformer.transform(request)
 
         self._service.add(add_language_dto)
@@ -42,14 +39,14 @@ class LanguageController(IController):
 
         return redirect(languages_url)
 
-    def edit_language_page_action(self) -> str:
+    def edit_language_page_action(self):
         view = EditLanguageView()
 
         view.set_data(language=self._service.find_by_code(request.args.get('code')))
 
         return view.render()
 
-    def edit_language_action(self) -> Response:
+    def edit_language_action(self):
         update_language_dto = UpdateLanguageDTOTransformer.transform(request)
 
         self._service.update(update_language_dto)
@@ -61,7 +58,7 @@ class LanguageController(IController):
 
         return redirect(languages_url)
 
-    def delete_language_action(self) -> Response:
+    def delete_language_action(self):
         self._service.delete_by_code(request.form.get('code'))
 
         languages_url = url_for(
--- app/controllers/ACP/PageController.py
+++ app/controllers/ACP/PageController.py
@@ -1,5 +1,4 @@
 from flask import g, redirect, request, url_for, abort
-from werkzeug import Response
 
 from blueprints.blueprint_names import ACP_PAGE_BLUEPRINT
 from controllers.IController import IController
@@ -19,18 +18,18 @@ from views.HTML.ACP.Page.PagesView import PagesView
 
 class PageController(IController):
 
-    def __init__(self, service: PageService, language_service: LanguageService) -> None:
+    def __init__(self, service: PageService, language_service: LanguageService):
         self._service = service
         self._language_service = language_service
 
-    def pages_page_action(self) -> str:
+    def pages_page_action(self):
         view = PagesView()
 
         view.set_data(pages=self._service.find_all())
 
         return view.render()
 
-    def page_page_action(self) -> Response | str:
+    def page_page_action(self):
         page_code = request.args.get('code')
 
         if page_code is None:
@@ -56,12 +55,12 @@ class PageController(IController):
 
         return view.render()
 
-    def add_page_page_action(self) -> str:
+    def add_page_page_action(self):
         view = AddPageView()
 
         return view.render()
 
-    def add_page_action(self) -> Response:
+    def add_page_action(self):
         add_page_dto = AddPageDTOTransformer.transform(request)
 
         self._service.add_page(add_page_dto)
@@ -73,7 +72,7 @@ class PageController(IController):
 
         return redirect(pages_url)
 
-    def edit_page_page_action(self) -> str:
+    def edit_page_page_action(self):
         view = EditPageView()
 
         page = self._service.find_by_code(request.args.get('code'))
@@ -82,7 +81,7 @@ class PageController(IController):
 
         return view.render()
 
-    def edit_page_action(self) -> Response:
+    def edit_page_action(self):
         update_page_dto = RequestToUpdatePageDTOTransformer.transform(request)
 
         self._service.update_page(update_page_dto)
@@ -94,7 +93,7 @@ class PageController(IController):
 
         return redirect(pages_url)
 
-    def delete_page_action(self) -> Response:
+    def delete_page_action(self):
         self._service.delete_by_code(request.form.get('code'))
 
         pages_url = url_for(
@@ -104,12 +103,12 @@ class PageController(IController):
 
         return redirect(pages_url)
 
-    def add_page_translation_page_action(self) -> str:
+    def add_page_translation_page_action(self):
         view = AddPageTranslationView()
 
         return view.render()
 
-    def add_page_translation_action(self) -> Response:
+    def add_page_translation_action(self):
         update_page_translation_dto = RequestToUpdatePageTranslationDTOTransformer.transform(request)
 
         self._service.update_page_translation(update_page_translation_dto)
@@ -121,7 +120,7 @@ class PageController(IController):
 
         return redirect(pages_url)
 
-    def edit_page_translation_page_action(self) -> str:
+    def edit_page_translation_page_action(self):
         view = EditPageTranslationView()
 
         translation = self._service.find_translation_by_id(int(request.args.get('id')))
@@ -130,7 +129,7 @@ class PageController(IController):
 
         return view.render()
 
-    def edit_page_translation_action(self) -> Response:
+    def edit_page_translation_action(self):
         update_page_translation_dto = RequestToUpdatePageTranslationDTOTransformer.transform(request)
 
         self._service.update_page_translation(update_page_translation_dto)
