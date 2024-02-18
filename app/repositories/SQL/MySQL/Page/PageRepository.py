6820cd772dcf391cce5b19071c4febf283c8fa49
Vadym Hutei <hutei@live.com>
2024-02-18 23:27:42 +0200

    page repository

--- app/repositories/SQL/MySQL/Page/PageRepository.py
+++ app/repositories/SQL/MySQL/Page/PageRepository.py
@@ -13,18 +13,21 @@ from transformers.entity_transformers.SQL.MySQL.Page.PageTextEntityTransformer i
 
 class PageRepository(IPageRepository, MySQLRepository):
 
-    mapper = PageMapper
-    text_mapper = PageTextMapper
-    transformer = PageEntityTransformer
-    translation_transformer = PageTextEntityTransformer
+    def __init__(self):
+        super().__init__()
+
+        self._mapper = PageMapper
+        self._text_mapper = PageTextMapper
+        self._transformer = PageEntityTransformer
+        self._text_transformer = PageTextEntityTransformer
 
     def find(self, entity_id: int) -> Entity | None:
         query = f'''
             SELECT
-                {self.mapper.fields}
-            FROM {self.mapper.table_as_prefix}
+                {self._mapper.fields}
+            FROM {self._mapper.table_as_prefix}
             WHERE
-                {self.mapper.pr_id_field} = {self.mapper.QUERY_PLACEHOLDER}
+                {self._mapper.pr_id_field} = {self._mapper.QUERY_PLACEHOLDER}
         '''
 
         query_data = (entity_id,)
@@ -34,17 +37,17 @@ class PageRepository(IPageRepository, MySQLRepository):
                 cursor.execute(query, query_data)
                 data = cursor.fetchone()
 
-        return self.transformer.transform(data) if data else None
+        return self._transformer.transform(data) if data else None
 
     def find_all(self) -> list[Entity]:
         query = f'''
             SELECT
-                {self.mapper.fields},
-                {self.text_mapper.fields}
-            FROM {self.mapper.table_as_prefix}
-            LEFT JOIN {self.text_mapper.table_as_prefix}
-                ON {self.text_mapper.entity_foreign_key_field_with_prefix} = {self.mapper.pr_field('id')}
-                AND {self.text_mapper.language_foreign_key_field_with_prefix} = {SQLEntityMapper.QUERY_PLACEHOLDER}
+                {self._mapper.fields},
+                {self._text_mapper.fields}
+            FROM {self._mapper.table_as_prefix}
+            LEFT JOIN {self._text_mapper.table_as_prefix}
+                ON {self._text_mapper.entity_foreign_key_field_with_prefix} = {self._mapper.pr_field('id')}
+                AND {self._text_mapper.language_foreign_key_field_with_prefix} = {SQLEntityMapper.QUERY_PLACEHOLDER}
         '''
 
         query_data = (g.current_language.id,)
@@ -54,15 +57,15 @@ class PageRepository(IPageRepository, MySQLRepository):
                 cursor.execute(query, query_data)
                 data = cursor.fetchall()
 
-        return self.transformer.transform_collection(data) if data else []
+        return self._transformer.transform_collection(data) if data else []
 
     def add(self, entity: Entity) -> bool:
         query = f'''
-              INSERT INTO {self.mapper.table} ({self.mapper.fillable_fields})
-              VALUES ({self.mapper.fillable_placeholders()})
+              INSERT INTO {self._mapper.table} ({self._mapper.fillable_fields})
+              VALUES ({self._mapper.fillable_placeholders()})
           '''
 
-        query_data = self.mapper.fillable_data(entity)
+        query_data = self._mapper.fillable_data(entity)
 
         with self.connection as connection:
             with connection.cursor() as cursor:
@@ -73,10 +76,10 @@ class PageRepository(IPageRepository, MySQLRepository):
         return result
 
     def update(self, entity: Entity) -> bool:
-        set_fields_statement, set_field_values = self.mapper.get_set_data(entity)
+        set_fields_statement, set_field_values = self._mapper.get_set_data(entity)
 
         query = f'''
-              UPDATE {self.mapper.table}
+              UPDATE {self._mapper.table}
               SET {set_fields_statement}
               WHERE id = {SQLEntityMapper.QUERY_PLACEHOLDER}
           '''
@@ -92,7 +95,7 @@ class PageRepository(IPageRepository, MySQLRepository):
         return result
 
     def delete(self, entity_id: int) -> bool:
-        query = f'DELETE FROM {self.mapper.table} WHERE id = {SQLEntityMapper.QUERY_PLACEHOLDER}'
+        query = f'DELETE FROM {self._mapper.table} WHERE id = {SQLEntityMapper.QUERY_PLACEHOLDER}'
 
         query_data = (entity_id,)
 
@@ -107,12 +110,12 @@ class PageRepository(IPageRepository, MySQLRepository):
     def find_translations_by_entity_code(self, entity_code: str) -> list[TextEntity]:
         query = f'''
             SELECT
-                {self.text_mapper.fields}
-            FROM {self.text_mapper.table_as_prefix}
-            JOIN {self.mapper.table_as_prefix}
-                ON {self.mapper.pr_id_field} = {self.text_mapper.entity_foreign_key_field_with_prefix}
+                {self._text_mapper.fields}
+            FROM {self._text_mapper.table_as_prefix}
+            JOIN {self._mapper.table_as_prefix}
+                ON {self._mapper.pr_id_field} = {self._text_mapper.entity_foreign_key_field_with_prefix}
             WHERE
-                {self.mapper.pr_field('code')} = {SQLEntityMapper.QUERY_PLACEHOLDER}
+                {self._mapper.pr_field('code')} = {SQLEntityMapper.QUERY_PLACEHOLDER}
         '''
 
         query_data = (entity_code,)
@@ -122,15 +125,15 @@ class PageRepository(IPageRepository, MySQLRepository):
                 cursor.execute(query, query_data)
                 data = cursor.fetchall()
 
-        return self.translation_transformer.transform_collection(data) if data else []
+        return self._text_transformer.transform_collection(data) if data else []
 
     def find_translation(self, entity_text_id: int) -> TextEntity | None:
         query = f'''
             SELECT
-                {self.text_mapper.fields}
-            FROM {self.text_mapper.table_as_prefix}
+                {self._text_mapper.fields}
+            FROM {self._text_mapper.table_as_prefix}
             WHERE
-                {self.text_mapper.table_prefix}.id = {SQLEntityMapper.QUERY_PLACEHOLDER}
+                {self._text_mapper.table_prefix}.id = {SQLEntityMapper.QUERY_PLACEHOLDER}
         '''
 
         query_data = (entity_text_id,)
@@ -140,17 +143,17 @@ class PageRepository(IPageRepository, MySQLRepository):
                 cursor.execute(query, query_data)
                 data = cursor.fetchone()
 
-        return self.translation_transformer.transform(data) if data else None
+        return self._text_transformer.transform(data) if data else None
 
     def find_translations_by_entity_id(self, entity_id: int) -> list[TextEntity]:
         query = f'''
             SELECT
-                {self.text_mapper.fields}
-            FROM {self.text_mapper.table_as_prefix}
-            JOIN {self.mapper.table_as_prefix}
-                ON {self.mapper.pr_id_field} = {self.text_mapper.entity_foreign_key_field_with_prefix}
+                {self._text_mapper.fields}
+            FROM {self._text_mapper.table_as_prefix}
+            JOIN {self._mapper.table_as_prefix}
+                ON {self._mapper.pr_id_field} = {self._text_mapper.entity_foreign_key_field_with_prefix}
             WHERE
-                {self.mapper.pr_field('id')} = {SQLEntityMapper.QUERY_PLACEHOLDER}
+                {self._mapper.pr_field('id')} = {SQLEntityMapper.QUERY_PLACEHOLDER}
         '''
 
         query_data = (entity_id,)
@@ -160,15 +163,15 @@ class PageRepository(IPageRepository, MySQLRepository):
                 cursor.execute(query, query_data)
                 data = cursor.fetchall()
 
-        return self.translation_transformer.transform_collection(data) if data else []
+        return self._text_transformer.transform_collection(data) if data else []
 
     def add_translation(self, text: TextEntity) -> bool:
         query = f'''
-            INSERT INTO {self.text_mapper.table} ({self.text_mapper.fillable_fields})
-            VALUES ({self.text_mapper.fillable_placeholders()})
+            INSERT INTO {self._text_mapper.table} ({self._text_mapper.fillable_fields})
+            VALUES ({self._text_mapper.fillable_placeholders()})
         '''
 
-        query_data = self.text_mapper.fillable_data(text)
+        query_data = self._text_mapper.fillable_data(text)
 
         with self.connection as connection:
             with connection.cursor() as cursor:
@@ -179,10 +182,10 @@ class PageRepository(IPageRepository, MySQLRepository):
         return result
 
     def update_translation(self, text: TextEntity) -> bool:
-        set_fields_statement, set_field_values = self.text_mapper.get_set_data(text)
+        set_fields_statement, set_field_values = self._text_mapper.get_set_data(text)
 
         query = f'''
-            UPDATE {self.text_mapper.table}
+            UPDATE {self._text_mapper.table}
             SET {set_fields_statement}
             WHERE id = {SQLEntityMapper.QUERY_PLACEHOLDER}
         '''
@@ -200,13 +203,13 @@ class PageRepository(IPageRepository, MySQLRepository):
     def find_by_code(self, code: str) -> Entity | None:
         query = f'''
             SELECT
-                {self.mapper.fields},
-                {self.text_mapper.fields}
-            FROM {self.mapper.table_as_prefix}
-            LEFT JOIN {self.text_mapper.table_as_prefix}
-                ON {self.text_mapper.entity_foreign_key_field_with_prefix} = {self.mapper.pr_id_field}
+                {self._mapper.fields},
+                {self._text_mapper.fields}
+            FROM {self._mapper.table_as_prefix}
+            LEFT JOIN {self._text_mapper.table_as_prefix}
+                ON {self._text_mapper.entity_foreign_key_field_with_prefix} = {self._mapper.pr_id_field}
             WHERE
-                {self.mapper.pr_field('code')} = {SQLEntityMapper.QUERY_PLACEHOLDER}
+                {self._mapper.pr_field('code')} = {SQLEntityMapper.QUERY_PLACEHOLDER}
         '''
 
         query_data = (code,)
@@ -216,10 +219,10 @@ class PageRepository(IPageRepository, MySQLRepository):
                 cursor.execute(query, query_data)
                 data = cursor.fetchone()
 
-        return self.transformer.transform(data) if data else None
+        return self._transformer.transform(data) if data else None
 
     def delete_by_code(self, code: str) -> bool:
-        query = f'DELETE FROM {self.mapper.table} WHERE code = {SQLEntityMapper.QUERY_PLACEHOLDER}'
+        query = f'DELETE FROM {self._mapper.table} WHERE code = {SQLEntityMapper.QUERY_PLACEHOLDER}'
 
         query_data = (code,)
 
@@ -234,9 +237,9 @@ class PageRepository(IPageRepository, MySQLRepository):
     def find_active(self) -> list[Entity]:
         query = f'''
             SELECT
-                {self.mapper.fields}
-            FROM {self.mapper.table_as_prefix}
-            WHERE {self.mapper.pr_field('is_active')} = 1
+                {self._mapper.fields}
+            FROM {self._mapper.table_as_prefix}
+            WHERE {self._mapper.pr_field('is_active')} = 1
         '''
 
         with self.connection as connection:
@@ -244,4 +247,4 @@ class PageRepository(IPageRepository, MySQLRepository):
                 cursor.execute(query)
                 data = cursor.fetchall()
 
-        return self.transformer.transform_collection(data) if data else []
+        return self._transformer.transform_collection(data) if data else []
