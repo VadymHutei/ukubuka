251d9153bc3f819c9132506f3c68fd5addbaa08f
Vadym Hutei <hutei@live.com>
2024-08-25 15:51:53 +0300

    home

--- app/modules/Home/controllers/HomeController.py
+++ app/modules/Home/controllers/HomeController.py
@@ -1,9 +1,14 @@
 from views.HTML.website.HomeView import HomeView
+from flask import current_app as app
 
 
 class HomeController:
 
     def home_page_action(self):
         view = HomeView()
+
+        view.set_data(
+            urls=app.url_map.iter_rules()
+        )
 
         return view.render()
